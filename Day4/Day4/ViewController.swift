395048935338d317581f19f2481d978f424d7fcc
Arunthekkootsasi7 <35930101+Arunthekkootsasi7@users.noreply.github.com>
2018-02-23 10:00:50 -0500

    Add files via upload

---
+++ Day4/Day4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Day4
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuItem = MenuItems()
+        tableView.rowHeight = 90
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
